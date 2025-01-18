9cb7b114ee2d2fd192f46c140697d806fa9e6245
Kateryna-4yka <kotasyri@gmail.com>
2025-01-18 19:51:09 +0200

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,100 +1,17 @@
+// У файлі pixabay-api.js зберігай функції для HTTP-запитів.
 
-import flatpickr from "flatpickr";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import "flatpickr/dist/flatpickr.min.css";
+export function nttpRequest (userRequest) {
+    return fetch (`https://pixabay.com/api/?q=${userRequest}&key=48292121-37734ba9461bf6417934fa015&image_type=photo&orientation=horizontal&safesearch=true`)
 
-
-const button = document.querySelector("[data-start]");
-button.disabled = true;
-
-const input = document.querySelector("#datetime-picker");
-input.disabled = false;
-const valueDays = document.querySelector("[data-days]");
-const valueHours = document.querySelector("[data-hours]");
-const valueMinutes = document.querySelector("[data-minutes]");
-const valueSeconds = document.querySelector("[data-seconds]");
-
-
-
-let userSelectedDate= null;
-let intervalID = null;
-
-// вибір валідної дати
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
+    //обробляємо відповідь що прийшла 
+    .then (queryResult=>{
+    //робимо перевірку, якщо інфа прийшла з помилкою, то перекидаємо у catch
+    if (!queryResult.ok)
+      {throw new Error(queryResult.status);}
         
-if (selectedDates[0] > options.defaultDate) {
-    button.disabled = false;
-    userSelectedDate = selectedDates[0]; 
-}
-else {iziToast.error({
-    position: 'topRight',
-    titleColor: 'white',
-    messageColor: 'white',
-    color: '#EF4040',
-    title: 'Error',
-    message: 'Please choose a date in the future',
-});
-    button.disabled = true;
-}},};
+    //а якщо все ок, тоді РОЗПАРШУЄМО данні для наступного then
+    return queryResult.json();
+    })
 
-const countdown = () => {
-    button.disabled = true;
-    input.disabled = true;
-
-
-intervalID = setInterval(()=>{
-const deff = userSelectedDate - Date.now();
-
-
-function convertMs(ms) {
-
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return {days, hours, minutes, seconds };
 
 }
-if (deff<0) {clearInterval(intervalID); 
-    input.disabled = false;
-
- } else {
-
-const content = convertMs(deff);
-
-if (content.days<10) { content.days = `0` + content.days}; 
-if (content.hours<10) { content.hours = `0` + content.hours}; 
-if (content.minutes<10) { content.minutes = `0` + content.minutes}; 
-if (content.seconds<10) { content.seconds = `0` + content.seconds}; 
-
-valueDays.textContent=content.days; 
-valueHours.textContent = content.hours; 
-valueMinutes.textContent = content.minutes;
-valueSeconds.textContent = content.seconds;
-
-
-}}, 1000);
-    
-       
-    }
-
-button.addEventListener ("click", countdown);
-
-flatpickr("#datetime-picker", options);
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,41 +1,35 @@
-
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+// У файлі render-functions.js створи функції для відображення елементів інтерфейсу.
 
 
-const form = document.querySelector(".form");
-const delay = document.querySelector(".delay>input");
-const fulfilled = document.querySelector(".fulfilled>input");
-const rejected = document.querySelector(".rejected>input");
+export function createCart (element) {
 
+    return ` 
+        <li class="li-gallery">
 
- form.addEventListener('submit', event => {
- event.preventDefault();
-let time = delay.value; 
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (fulfilled.checked) 
-        {resolve (time)} 
-      else {reject(time)}
-    }, time)
-  })
+        <a class="li-gallery-a" href="${element.largeImageURL}">
+        <img class="img" src="${element.webformatURL}" 
+        alt="${element.tags}" 
+        />
+        </a>
+        <ul class="baner-info">
+          <li class="baner-info-li">
+            <p class="baner-info-li-title">Likes</p>
+            <p class="baner-info-li-text">${element.likes}</p>
+          </li>
+          <li class="baner-info-li">
+            <p class="baner-info-li-title">Views</p>
+            <p class="baner-info-li-text">${element.views}</p>
+          </li>
+          <li class="baner-info-li">
+            <p class="baner-info-li-title">Comments</p>
+            <p class="baner-info-li-text">${element.comments}</p>
+          </li>
+          <li class="baner-info-li">
+            <p class="baner-info-li-title">Downloads</p>
+            <p class="baner-info-li-text">${element.downloads}</p>
+          
+            </li>
+        </ul>
+        </li> `;
+}
 
-  .then((delay) => {
-    iziToast.success({
-      position: 'topRight',
-      message: `✅ Fulfilled promise in ${delay} ms`,
-  });})
-
-
-  .catch(delay => {
-    iziToast.error({
-      position: 'topRight',
-      message: `❌ Rejected promise in ${delay} ms`,
-  });}) 
-
-    .finally (()=> {    
-      delay.value = "";
-      rejected.checked = false;
-      fulfilled.checked = false;})
-
-});
